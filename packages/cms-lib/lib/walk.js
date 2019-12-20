ddbb649530df02ecc718ecd3376203dd606a75a3
Mike Talley <mtalley@hubspot.com>
2019-12-20 11:22:35 -0500

    processFiles no resolve with data object with stat type and are handled appropriately in reducer

--- packages/cms-lib/lib/walk.js
+++ packages/cms-lib/lib/walk.js
@@ -1,47 +1,74 @@
 const fs = require('fs');
 const path = require('path');
 const { logger } = require('../logger');
+const { logFileSystemErrorInstance } = require('../errorHandlers');
+
+const STAT_TYPES = {
+  FILE: 'file',
+  SYMBOLIC_LINK: 'symlink',
+  DIRECTORY: 'dir',
+};
+
+const generateFilePromise = (dir, file) => {
+  return new Promise((resolve, reject) => {
+    const filepath = path.join(dir, file);
+    fs.lstat(filepath, (error, stats) => {
+      if (error) {
+        reject(error);
+      }
+      if (stats.isSymbolicLink()) {
+        resolve({
+          filepath,
+          type: STAT_TYPES.SYMBOLIC_LINK,
+        });
+      }
+      if (stats.isDirectory()) {
+        walk(filepath).then(fileContents => {
+          return resolve({
+            filepath,
+            type: STAT_TYPES.DIRECTORY,
+            fileContents,
+          });
+        });
+      } else if (stats.isFile()) {
+        resolve({
+          filepath,
+          type: STAT_TYPES.FILE,
+        });
+      }
+    });
+  }).catch(logger.error);
+};
+
+const filesDataReducer = (all, fileData) => {
+  try {
+    switch (fileData.type) {
+      case STAT_TYPES.FILE:
+        return all.concat(fileData.filepath);
+      case STAT_TYPES.DIRECTORY:
+        return all.concat(fileData.fileContents);
+      case STAT_TYPES.SYMBOLIC_LINK:
+        // Skip symlinks
+        return all;
+      default:
+        return all;
+    }
+  } catch (e) {
+    logFileSystemErrorInstance(e, fileData);
+  }
+};
 
 function walk(dir) {
   const processFiles = files => {
-    return Promise.all(
-      files.map(file => {
-        return new Promise((resolve, reject) => {
-          const filepath = path.join(dir, file);
-          fs.lstat(filepath, (error, stats) => {
-            if (error) {
-              reject(error);
-            }
-            if (stats.isSymbolicLink()) {
-              logger.debug(`Skipping Symlink in walk: ${filepath}`);
-              reject(error);
-            }
-            if (stats.isDirectory()) {
-              walk(filepath).then(resolve);
-            } else if (stats.isFile()) {
-              resolve(filepath);
-            }
-          });
-        }).catch(error => {
-          if (error) {
-            logger.error(error);
-          }
-        });
-      })
-    );
+    return Promise.all(files.map(file => generateFilePromise(dir, file)));
   };
   return new Promise((resolve, reject) => {
     fs.readdir(dir, (error, files) => {
       if (error) {
         reject(error);
       }
-      processFiles(files).then(foldersContents => {
-        resolve(
-          foldersContents.reduce(
-            (all, folderContents) => all.concat(folderContents),
-            []
-          )
-        );
+      processFiles(files).then(filesData => {
+        resolve(filesData.reduce(filesDataReducer, []));
       });
     });
   });
