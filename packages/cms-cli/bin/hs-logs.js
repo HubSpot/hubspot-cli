605986ded7bf4baa319dbc671e90978887fb5f43
Mike Talley <mtalley@hubspot.com>
2020-01-15 17:41:42 -0500

    fixed usage of command

--- packages/cms-cli/bin/hs-logs.js
+++ packages/cms-cli/bin/hs-logs.js
@@ -7,3 +7,7 @@ const { getLogs } = require('../commands/logs');
 const program = new Command('hs logs');
 getLogs(program);
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.help();
+}
--- packages/cms-cli/bin/hscms-logs.js
+++ packages/cms-cli/bin/hscms-logs.js
@@ -7,3 +7,7 @@ const { getLogs } = require('../commands/logs');
 const program = new Command('hscms logs');
 getLogs(program);
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.help();
+}
