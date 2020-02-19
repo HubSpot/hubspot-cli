ea7ad291ee4b1860ca64f192f56b0ffbc0f3904d
Mike Talley <mtalley@hubspot.com>
2020-02-19 14:25:08 -0500

    corrected reference to fileMapper PROD variable

--- packages/cms-lib/api/fileMapper.js
+++ packages/cms-lib/api/fileMapper.js
@@ -4,7 +4,7 @@ const contentDisposition = require('content-disposition');
 const http = require('../http');
 const { getCwd } = require('../path');
 const { getAndLoadConfigIfNeeded, getPortalConfig } = require('../lib/config');
-const { PROD } = require('../lib/environment');
+const { PROD } = require('../lib/constants/environment');
 const { logger } = require('../logger');
 
 const FILE_MAPPER_API_PATH = 'content/filemapper/v1';
