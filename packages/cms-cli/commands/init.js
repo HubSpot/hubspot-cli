bae1c534caadc60315381349b152b0688f9f3da0
Gregory Cornelius <gcornelius@hubspot.com>
2020-02-25 08:06:03 -0500

    Use access key authentication during the init flow

--- packages/cms-cli/commands/init.js
+++ packages/cms-cli/commands/init.js
@@ -1,22 +1,13 @@
 const { version } = require('../package.json');
-const {
-  getConfigPath,
-  updatePortalConfig,
-  updateDefaultPortal,
-  createEmptyConfigFile,
-  deleteEmptyConfigFile,
-} = require('@hubspot/cms-lib/lib/config');
+const { getConfigPath } = require('@hubspot/cms-lib/lib/config');
 const {
   logFileSystemErrorInstance,
 } = require('@hubspot/cms-lib/errorHandlers');
 const {
   DEFAULT_HUBSPOT_CONFIG_YAML_FILE_NAME,
-  AUTH_METHODS,
   PERSONAL_ACCESS_KEY_AUTH_METHOD,
 } = require('@hubspot/cms-lib/lib/constants');
-const { handleExit } = require('@hubspot/cms-lib/lib/process');
 const { logger } = require('@hubspot/cms-lib/logger');
-const { authenticateWithOauth } = require('@hubspot/cms-lib/oauth');
 const {
   personalAccessKeyPrompt,
   updateConfigWithPersonalAccessKey,
@@ -26,12 +17,6 @@ const {
   addHelpUsageTracking,
   trackAuthAction,
 } = require('../lib/usageTracking');
-const {
-  promptUser,
-  OAUTH_FLOW,
-  API_KEY_FLOW,
-  AUTH_METHOD,
-} = require('../lib/prompts');
 const { addLoggerOptions, setLogLevel } = require('../lib/commonOpts');
 const { logDebugInfo } = require('../lib/debugInfo');
 
@@ -40,60 +25,6 @@ const TRACKING_STATUS = {
   STARTED: 'started',
   COMPLETE: 'complete',
 };
-const AUTH_METHOD_FLOW = {
-  [AUTH_METHODS.api.value]: {
-    prompt: async () => {
-      return promptUser(API_KEY_FLOW);
-    },
-    setup: async configData => {
-      createEmptyConfigFile();
-      handleExit(deleteEmptyConfigFile);
-      updateDefaultPortal(configData.name);
-      updatePortalConfig({
-        ...configData,
-        authType: AUTH_METHODS.api.value,
-      });
-      logger.log(
-        `Success: ${DEFAULT_HUBSPOT_CONFIG_YAML_FILE_NAME} created with ${AUTH_METHODS.api.name}.`
-      );
-    },
-  },
-  [AUTH_METHODS.oauth.value]: {
-    prompt: async () => {
-      return promptUser(OAUTH_FLOW);
-    },
-    setup: async configData => {
-      createEmptyConfigFile();
-      handleExit(deleteEmptyConfigFile);
-      await authenticateWithOauth(configData);
-      logger.log(
-        `Success: ${DEFAULT_HUBSPOT_CONFIG_YAML_FILE_NAME} created with ${AUTH_METHODS.oauth.name}.`
-      );
-      process.exit();
-    },
-  },
-  [PERSONAL_ACCESS_KEY_AUTH_METHOD.value]: {
-    prompt: personalAccessKeyPrompt,
-    setup: async configData => {
-      await updateConfigWithPersonalAccessKey(configData, true);
-    },
-  },
-};
-
-const completeConfigSetup = async ({ authMethod, configPath }) => {
-  const flow = AUTH_METHOD_FLOW[authMethod];
-  trackAuthAction(COMMAND_NAME, authMethod, TRACKING_STATUS.STARTED);
-
-  try {
-    flow.setup(await flow.prompt());
-  } catch (err) {
-    logFileSystemErrorInstance(err, {
-      filepath: configPath,
-    });
-  }
-
-  trackAuthAction(COMMAND_NAME, authMethod, TRACKING_STATUS.COMPLETE);
-};
 
 function initializeConfigCommand(program) {
   program
@@ -101,31 +32,38 @@ function initializeConfigCommand(program) {
     .description(
       `initialize ${DEFAULT_HUBSPOT_CONFIG_YAML_FILE_NAME} for a HubSpot portal`
     )
-    .option('--personal-access-key', 'utilize personal CMS access key for auth')
     .action(async options => {
       setLogLevel(options);
       logDebugInfo(options);
       trackCommandUsage(COMMAND_NAME);
 
       const configPath = getConfigPath();
-      let authMethod;
 
       if (configPath) {
         logger.error(`The config file '${configPath}' already exists.`);
         process.exit(1);
       }
+      trackAuthAction(
+        COMMAND_NAME,
+        PERSONAL_ACCESS_KEY_AUTH_METHOD.value,
+        TRACKING_STATUS.STARTED
+      );
 
-      if (options.personalAccessKey) {
-        authMethod = PERSONAL_ACCESS_KEY_AUTH_METHOD.value;
-      } else {
-        const promptResp = await promptUser(AUTH_METHOD);
-        authMethod = promptResp.authMethod;
+      try {
+        const configData = await personalAccessKeyPrompt();
+        await updateConfigWithPersonalAccessKey(configData, true);
+      } catch (err) {
+        console.log(err);
+        logFileSystemErrorInstance(err, {
+          filepath: configPath,
+        });
       }
 
-      await completeConfigSetup({
-        authMethod,
-        configPath,
-      });
+      trackAuthAction(
+        COMMAND_NAME,
+        PERSONAL_ACCESS_KEY_AUTH_METHOD.value,
+        TRACKING_STATUS.COMPLETE
+      );
     });
 
   addLoggerOptions(program);
