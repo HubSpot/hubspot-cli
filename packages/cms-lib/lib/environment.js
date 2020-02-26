2837a0caef47d611b0e2954f17cda732011f8653
Mike Talley <mtalley@hubspot.com>
2020-02-26 14:08:15 -0500

    simplified logic of getValidEnv

--- packages/cms-lib/lib/environment.js
+++ packages/cms-lib/lib/environment.js
@@ -3,8 +3,8 @@ const { QA, PROD } = require('./constants');
 /**
  * Returns environment constant for QA and PROD or optional masked value for PROD
  * @param {string} env Environment string, can be any case
- * @param {(boolean|object)} shouldMaskProduction Returning alternate value for 'prod' -- Can be used to hide env value in the config
- * @param {any} shouldMaskProduction.maskedValue Alternate value to return in place of 'prod'
+ * @param {(boolean|object)} shouldMaskProduction Returning alternate value for PROD -- Can be used to hide env value in the config
+ * @param {any} shouldMaskProduction.maskedValue Alternate value to return in place of PROD
  */
 const getValidEnv = (env, shouldMaskProduction) => {
   const maskValue =
@@ -12,13 +12,12 @@ const getValidEnv = (env, shouldMaskProduction) => {
     Object.prototype.hasOwnProperty.call(shouldMaskProduction, 'maskedValue')
       ? shouldMaskProduction.maskValue
       : '';
+  const prodValue = shouldMaskProduction ? maskValue : PROD;
 
   const returnVal =
     typeof env && typeof env === 'string' && env.toLowerCase() === QA
       ? QA
-      : shouldMaskProduction
-      ? maskValue
-      : PROD;
+      : prodValue;
 
   return returnVal;
 };
