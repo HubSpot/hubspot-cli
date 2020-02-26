807e6bc575814cf685f45c7bcf194979aae7c3e4
Mike Talley <mtalley@hubspot.com>
2020-02-26 10:38:32 -0500

    created variable for shared errorOutput string

--- packages/cms-lib/personalAccessKey.js
+++ packages/cms-lib/personalAccessKey.js
@@ -33,14 +33,13 @@ async function getAccessToken(personalAccessKey, env = 'PROD') {
     response = await fetchAccessToken(personalAccessKey, env);
   } catch (e) {
     if (e.response) {
+      const errorOutput = `Error while retrieving new access token: ${e.response.body.message}.`;
       if (e.response.statusCode === 401) {
         throw new HubSpotAuthError(
-          `Error while retrieving new access token: ${e.response.body.message}. \nYour personal CMS access key is invalid. Please run "hs auth personalaccesskey" to reauthenticate. See https://designers.hubspot.com/docs/personal-access-keys for more information.`
+          `${errorOutput} \nYour personal CMS access key is invalid. Please run "hs auth personalaccesskey" to reauthenticate. See https://designers.hubspot.com/docs/personal-access-keys for more information.`
         );
       } else {
-        throw new HubSpotAuthError(
-          `Error while retrieving new access token: ${e.response.body.message}`
-        );
+        throw new HubSpotAuthError(errorOutput);
       }
     } else {
       throw e;
