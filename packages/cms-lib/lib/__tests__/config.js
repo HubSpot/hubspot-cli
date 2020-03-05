73a9b0e97ccf803357897ececa2bbd5530a98ca9
Mike Talley <mtalley@hubspot.com>
2020-03-05 10:22:25 -0500

    added tests for loading configs from environment

--- packages/cms-lib/lib/__tests__/config.js
+++ packages/cms-lib/lib/__tests__/config.js
@@ -1,11 +1,49 @@
+const fs = require('fs');
+const configModule = require('../config');
 const {
   setConfig,
+  getAndLoadConfigIfNeeded,
   getConfig,
   getPortalId,
   updateDefaultPortal,
   deleteEmptyConfigFile,
-} = require('../config');
+} = configModule;
 jest.mock('fs');
+
+const API_KEY_CONFIG = {
+  name: 'API',
+  portalId: 1111,
+  authType: 'apikey',
+  apiKey: 'secret',
+};
+const OAUTH2_CONFIG = {
+  name: 'OAUTH2',
+  portalId: 2222,
+  authType: 'oauth2',
+  auth: {
+    clientId: 'fakeClientId',
+    clientSecret: 'fakeClientSecret',
+    scopes: ['content'],
+    tokenInfo: {
+      expiresAt: '2020-01-01T00:00:00.000Z',
+      refreshToken: 'fakeOauthRefreshToken',
+      accessToken: 'fakeOauthAccessToken',
+    },
+  },
+};
+const PERSONAL_ACCESS_KEY_CONFIG = {
+  name: 'PERSONALACCESSKEY',
+  portalId: 3333,
+  authType: 'personalaccesskey',
+  auth: {
+    scopes: ['content'],
+    tokenInfo: {
+      expiresAt: '2020-01-01T00:00:00.000Z',
+      accessToken: 'fakePersonalAccessKeyAccessToken',
+    },
+  },
+  personalAccessKey: 'fakePersonalAccessKey',
+};
 
 describe('lib/config', () => {
   describe('getPortalId()', () => {
@@ -71,6 +109,140 @@ describe('lib/config', () => {
 
       deleteEmptyConfigFile();
       expect(fs.unlinkSync).toHaveBeenCalled();
+    });
+  });
+
+  describe('getAndLoadConfigIfNeeded)', () => {
+    beforeEach(() => {
+      setConfig(null);
+      process.env = {};
+    });
+
+    it('loads a config from file if no combination of environment variables is sufficient', () => {
+      const spy = jest.spyOn(configModule, 'getConfigPath');
+
+      getAndLoadConfigIfNeeded();
+      expect(configModule.getConfigPath).not.toHaveBeenCalled();
+      spy.mockRestore();
+    });
+
+    describe('oauth environment variable config', () => {
+      const {
+        portalId,
+        auth: {
+          clientId,
+          clientSecret,
+          tokenInfo: { refreshToken },
+        },
+      } = OAUTH2_CONFIG;
+      let portalConfig;
+
+      beforeEach(() => {
+        process.env = {
+          HUBSPOT_PORTAL_ID: portalId,
+          HUBSPOT_CLIENT_ID: clientId,
+          HUBSPOT_CLIENT_SECRET: clientSecret,
+          HUBSPOT_REFRESH_TOKEN: refreshToken,
+        };
+        getAndLoadConfigIfNeeded();
+        portalConfig = getConfig().portals[0];
+      });
+
+      it('does not load a config from file', () => {
+        expect(fs.readFileSync).not.toHaveBeenCalled();
+      });
+
+      it('creates a portal config', () => {
+        expect(portalConfig).toBeTruthy();
+      });
+
+      it('sets the portal as default', () => {
+        expect(getConfig().defaultPortal).toEqual(portalConfig.name);
+      });
+
+      it('properly loads portal id value', () => {
+        expect(portalConfig.portalId).toEqual(portalId);
+      });
+
+      it('properly loads client id value', () => {
+        expect(portalConfig.auth.clientId).toEqual(clientId);
+      });
+
+      it('properly loads client secret value', () => {
+        expect(portalConfig.auth.clientSecret).toEqual(clientSecret);
+      });
+
+      it('properly loads refresh token value', () => {
+        expect(portalConfig.auth.tokenInfo.refreshToken).toEqual(refreshToken);
+      });
+    });
+
+    describe('apikey environment variable config', () => {
+      const { portalId, apiKey } = API_KEY_CONFIG;
+      let portalConfig;
+
+      beforeEach(() => {
+        process.env = {
+          HUBSPOT_PORTAL_ID: portalId,
+          HUBSPOT_API_KEY: apiKey,
+        };
+        getAndLoadConfigIfNeeded();
+        portalConfig = getConfig().portals[0];
+      });
+
+      it('does not load a config from file', () => {
+        expect(fs.readFileSync).not.toHaveBeenCalled();
+      });
+
+      it('creates a portal config', () => {
+        expect(portalConfig).toBeTruthy();
+      });
+
+      it('sets the portal as default', () => {
+        expect(getConfig().defaultPortal).toEqual(portalConfig.name);
+      });
+
+      it('properly loads portal id value', () => {
+        expect(portalConfig.portalId).toEqual(portalId);
+      });
+
+      it('properly loads api key value', () => {
+        expect(portalConfig.apiKey).toEqual(apiKey);
+      });
+    });
+
+    describe('personalaccesskey environment variable config', () => {
+      const { portalId, personalAccessKey } = PERSONAL_ACCESS_KEY_CONFIG;
+      let portalConfig;
+
+      beforeEach(() => {
+        process.env = {
+          HUBSPOT_PORTAL_ID: portalId,
+          HUBSPOT_PERSONAL_ACCESS_KEY: personalAccessKey,
+        };
+        getAndLoadConfigIfNeeded();
+        portalConfig = getConfig().portals[0];
+      });
+
+      it('does not load a config from file', () => {
+        expect(fs.readFileSync).not.toHaveBeenCalled();
+      });
+
+      it('creates a portal config', () => {
+        expect(portalConfig).toBeTruthy();
+      });
+
+      it('sets the portal as default', () => {
+        expect(getConfig().defaultPortal).toEqual(portalConfig.name);
+      });
+
+      it('properly loads portal id value', () => {
+        expect(portalConfig.portalId).toEqual(portalId);
+      });
+
+      it('properly loads personal access key value', () => {
+        expect(portalConfig.personalAccessKey).toEqual(personalAccessKey);
+      });
     });
   });
 });
