b0b796cb9d8ca9977f6bcd57450d4ef4f22c7341
Mike Talley <mtalley@hubspot.com>
2020-03-19 10:11:55 -0400

    help will now be displayed for commands that require an argument when none are passed

--- packages/cms-cli/bin/hscms-rm.js
+++ packages/cms-cli/bin/hscms-rm.js
@@ -6,3 +6,7 @@ const { configureRemoveCommand } = require('../commands/remove');
 const program = new Command(`hscms rm`);
 configureRemoveCommand(program);
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.help();
+}
