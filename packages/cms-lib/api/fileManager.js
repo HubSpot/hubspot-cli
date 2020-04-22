326f610bd7ce95eef566b83756c13b970a0eb93d
Gregory Cornelius <gcornelius@hubspot.com>
2020-04-22 09:20:48 -0400

    Fix uploading a file to file manager root

--- packages/cms-lib/api/fileManager.js
+++ packages/cms-lib/api/fileManager.js
@@ -7,17 +7,21 @@ const FILE_MANAGER_API_PATH = 'filemanager/api/v2';
 async function uploadFile(portalId, src, dest) {
   const directory = path.dirname(dest);
   const filename = path.basename(dest);
+  const formData = {
+    file: fs.createReadStream(src),
+    file_names: filename,
+  };
+
+  if (directory && directory !== '.') {
+    formData.folder_paths = directory;
+  }
 
   return http.post(portalId, {
     uri: `${FILE_MANAGER_API_PATH}/files`,
     qs: {
       overwrite: 'true',
     },
-    formData: {
-      file: fs.createReadStream(src),
-      file_names: filename,
-      folder_paths: directory,
-    },
+    formData,
   });
 }
 
