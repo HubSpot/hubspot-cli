28377a5fef38473c245fa752b599e957747da957
Mike Talley <mtalley@hubspot.com>
2020-05-14 15:48:20 -0400

    got hs qa commands working

--- packages/cms-cli/bin/hs-qa-auth.js
+++ packages/cms-cli/bin/hs-qa-auth.js
@@ -7,3 +7,7 @@ const { qaAuthCommand } = require('../commands/qa');
 const program = new Command('hs qa auth');
 qaAuthCommand(program);
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.help();
+}
--- packages/cms-cli/commands/qa.js
+++ packages/cms-cli/commands/qa.js
@@ -1,5 +1,3 @@
-const { initializeConfigCommand } = require('../commands/init');
-const { configureAuthCommand } = require('../commands/auth');
 const { initAction } = require('./init');
 const { authAction } = require('./auth');
 const {
@@ -8,34 +6,34 @@ const {
   SUPPORTED_AUTHENTICATION_PROTOCOLS_TEXT,
 } = require('@hubspot/cms-lib/lib/constants');
 
+const initDescription = `Initialize ${DEFAULT_HUBSPOT_CONFIG_YAML_FILE_NAME} for a HubSpot portal in ${ENVIRONMENTS.QA} environment.`;
+const authDescription = `Configure authentication for a HubSpot account in ${ENVIRONMENTS.QA} environment. Supported authentication protocols are ${SUPPORTED_AUTHENTICATION_PROTOCOLS_TEXT}.`;
+
 function qaCommand(program) {
   program
     .description('Run commands with configuration for QA environment')
-    .command(
-      'init',
-      `initialize ${DEFAULT_HUBSPOT_CONFIG_YAML_FILE_NAME} for a HubSpot portal in ${ENVIRONMENTS.QA} environment`
-    )
-    .command(
-      'auth',
-      `Configure authentication for a HubSpot account in ${ENVIRONMENTS.QA} environment. Supported authentication protocols are ${SUPPORTED_AUTHENTICATION_PROTOCOLS_TEXT}.`
-    );
+    .command('init', initDescription)
+    .command('auth', authDescription);
 }
 
 function qaInitCommand(program) {
-  program
-    .description('run commands with configuration for QA environment')
-    .action(async options => {
-      initializeConfigCommand(program);
-      initAction(options);
+  program.description(initDescription).action(async options => {
+    initAction({
+      ...options,
+      qa: true,
     });
+  });
 }
 
 function qaAuthCommand(program) {
   program
-    .description('run commands with configuration for QA environment')
-    .action(async options => {
-      configureAuthCommand(program);
-      authAction(options);
+    .description(authDescription)
+    .arguments('<type>')
+    .action(async (type, options) => {
+      authAction(type, {
+        ...options,
+        qa: true,
+      });
     });
 }
 
