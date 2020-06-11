5b68935cb179bffc739b61c717818f805ec96c1c
Anthony Pizzurro <apizzurro@hubspot.com>
2020-06-11 15:53:25 -0400

    mock path

--- packages/cms-lib/__tests__/hubdb.js
+++ packages/cms-lib/__tests__/hubdb.js
@@ -1,5 +1,7 @@
 const { downloadHubDbTable } = require('../hubdb');
+const { getCwd } = require('../path');
 
+jest.mock('../path');
 jest.mock('../api/hubdb');
 
 describe('cms-lib/api/hubdb', () => {
@@ -7,9 +9,12 @@ describe('cms-lib/api/hubdb', () => {
     const portalId = 123;
     const tableId = 456;
     const destPath = 'tmp.json';
+    const projectCwd = '/home/tom/projects';
+
+    getCwd.mockReturnValue(projectCwd);
 
     const { filePath } = await downloadHubDbTable(portalId, tableId, destPath);
 
-    expect(filePath).toEqual(destPath);
+    expect(filePath).toEqual(`${projectCwd}/${destPath}`);
   });
 });
