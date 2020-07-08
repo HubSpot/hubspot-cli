80f10a11b205a83e0475ce88bea9ed6436d4e710
Anthony Pizzurro <apizzurro@hubspot.com>
2020-07-08 12:44:16 -0400

    look for serverless.json instead of dir

--- packages/cms-lib/functions.js
+++ packages/cms-lib/functions.js
@@ -4,6 +4,7 @@ const findup = require('findup-sync');
 const { logger } = require('./logger');
 const { logFileSystemErrorInstance } = require('./errorHandlers');
 const isObject = require('./lib/isObject');
+const { getCwd } = require('./path');
 
 const functionBody = `
 exports.main = ({ accountId, body, params }, sendResponse) => {
@@ -98,7 +99,10 @@ function createFunction(
   { functionsFolder, filename, endpointPath, endpointMethod },
   dest
 ) {
-  const ancestorFunctionsDir = findup('*.functions');
+  const ancestorFunctionsDir = findup('serverless.json', {
+    cwd: getCwd(),
+    nocase: true,
+  });
 
   if (ancestorFunctionsDir) {
     logger.error(
