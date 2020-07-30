35b5ba051b5f9d652b90dccbaba761f7d0d3f920
Anthony Pizzurro <apizzurro@hubspot.com>
2020-07-30 12:47:46 -0400

    fix casing

--- packages/cms-cli/lib/createTemplatePrompt.js
+++ packages/cms-cli/lib/createTemplatePrompt.js
@@ -4,7 +4,7 @@ const TEMPLATE_TYPE_PROMPT = {
   type: 'list',
   name: 'templateType',
   message: 'Select the type of template to create',
-  default: 'PAGE',
+  default: 'page',
   choices: [
     { name: 'page', value: 'page-template' },
     { name: 'email', value: 'email-template' },
