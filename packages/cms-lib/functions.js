6ad05df9c87e7c539a7e80b3b0c4b9c104c1b800
Gregory Cornelius <gcornelius@hubspot.com>
2020-09-01 14:11:48 -0400

    Update serverless function boilerplate to match Design Manager
    Fixes #275

--- packages/cms-lib/functions.js
+++ packages/cms-lib/functions.js
@@ -7,14 +7,34 @@ const isObject = require('./lib/isObject');
 const { getCwd } = require('./path');
 
 const functionBody = `
-exports.main = ({ accountId, body, params }, sendResponse) => {
-  console.log('Your HubSpot account ID: %i', accountId);
-  sendResponse({
-    statusCode: 200,
-    body: {
-      message: 'Hello, world!',
-    },
-  });
+// Require axios library to make API requests
+const axios = require('axios');
+
+// This function is executed when a request is made to the endpoint associated with this file in the serverless.json file
+exports.main = ({ accountId }, sendResponse) => {
+  // Use axios to make a GET request to the search API
+  axios
+    .get('https://api.hubapi.com/contentsearch/v2/search', {
+      params: {
+        portalId: accountId,
+        term: 'searchTerm',
+      },
+    })
+    .then(function(response) {
+      // Handle success
+      // The console.log statement will appear in the terminal when you run the hs logs CMS CLI command
+      // For full documentation, see: developers.hubspot.com/docs/cms/developer-reference/local-development-cms-cli#logs
+      console.log('Data received from the search API:', response.data);
+      // sendResponse is what you will send back to services hitting your serverless function
+      sendResponse({ body: { response: response }, statusCode: 200 });
+    })
+    .catch(function(error) {
+      // Handle error
+
+      // This is a simple example; error handling typically will be more complicated.
+      // For more information on error handling with axios, see: https://github.com/axios/axios#handling-errors
+      sendResponse({ body: { error: error.message }, statusCode: 500 });
+    });
 };
 `;
 
