e76238e5fd9771553ac112cae29d4c74af188f7f
Andrew Jenkins <ajenkins@hubspot.com>
2020-09-03 12:18:50 -0400

    Code review changes.  Alias hs.js to cli.js and remove the hardcoded script name since we don't need it anymore

--- packages/cms-cli/bin/hs.js
+++ packages/cms-cli/bin/hs.js
@@ -1,34 +1,3 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-const updateNotifier = require('update-notifier');
-
-const { logger } = require('@hubspot/cms-lib/logger');
-const { configureMainCommand } = require('../commands/main');
-const { setLogLevel } = require('../lib/commonOpts');
-
-const pkg = require('../package.json');
-
-const notifier = updateNotifier({ pkg });
-
-notifier.notify({
-  shouldNotifyInNpmScript: true,
-});
-
-const showCommandNotFoundError = program => {
-  setLogLevel(program);
-  const { args } = program;
-  logger.error('The command "hs %s" is not a valid command', args[0]);
-  logger.log('Run "hs --help" for a list of available commands');
-  process.exit();
-};
-
-const program = new Command();
-configureMainCommand(program);
-const result = program.parse(process.argv);
-
-// Git-style subcommands return `undefined` when parsed so we leverage that
-// to detect when a user attempts to run a command that doesn't exist
-if (result && !program.runningCommand) {
-  showCommandNotFoundError(program);
-}
+require('./cli');
