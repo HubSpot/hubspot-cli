2566f3cd6294b80342ff310da0f765e16c675fd9
Mike Talley <mtalley@hubspot.com>
2020-09-15 15:13:54 -0400

    updated all `open` versions to `^7.0.3` and added `{ url: true }` option for URL use

--- packages/cms-cli/lib/oauth.js
+++ packages/cms-cli/lib/oauth.js
@@ -31,7 +31,7 @@ const handleServerOnProcessEnd = server => {
 };
 
 const authorize = async oauthManager => {
-  open(buildAuthUrl(oauthManager));
+  open(buildAuthUrl(oauthManager), { url: true });
 
   // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
