a8e2b09cbb170ec2d3510f34859e327da890f2c2
Andrew Jenkins <ajenkins@hubspot.com>
2020-09-29 14:10:28 -0400

    Add some tests

--- packages/cms-lib/__tests__/schema.js
+++ packages/cms-lib/__tests__/schema.js
@@ -1,7 +1,11 @@
-const { cleanSchema } = require('../schema');
+const { cleanSchema, writeSchemaToDisk, logSchemas } = require('../schema');
 const basic = require('./fixtures/schema/basic.json');
 const full = require('./fixtures/schema/full.json');
+const { getCwd } = require('@hubspot/cms-lib/path');
 const multiple = require('./fixtures/schema/multiple.json');
+const fs = require('fs-extra');
+const path = require('path');
+const { logger } = require('@hubspot/cms-lib/logger');
 
 describe('cms-lib/schema', () => {
   describe('cleanSchema()', () => {
@@ -15,6 +19,25 @@ describe('cms-lib/schema', () => {
 
     it('cleans multiple schema', () => {
       expect(multiple.map(cleanSchema)).toMatchSnapshot();
+    });
+  });
+
+  describe('writeSchemaToDisk()', () => {
+    it('writes schema to disk', () => {
+      const spy = jest.spyOn(fs, 'outputFileSync');
+      expect(fs.existsSync(path.resolve(getCwd(), `${basic.name}.json`))).toBe(
+        false
+      );
+      writeSchemaToDisk(basic);
+      expect(spy.mock.calls[0][1]).toMatchSnapshot();
+    });
+  });
+
+  describe('logSchemas()', () => {
+    it('logs schemas', () => {
+      const spy = jest.spyOn(logger, 'log');
+      logSchemas([basic]);
+      expect(spy.mock.calls[0][0]).toMatchSnapshot();
     });
   });
 });
--- packages/cms-lib/schema.js
+++ packages/cms-lib/schema.js
@@ -81,4 +81,5 @@ module.exports = {
   cleanSchema,
   downloadSchemas,
   downloadSchema,
+  logSchemas,
 };
