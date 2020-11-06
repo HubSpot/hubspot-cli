665036cbca362680ae1a4c961e923b291c9a7bf1
Anthony Pizzurro <apizzurro@hubspot.com>
2020-11-06 15:15:03 -0500

    pass query through with updated limit

--- packages/cms-lib/api/results.js
+++ packages/cms-lib/api/results.js
@@ -3,14 +3,11 @@ const http = require('../http');
 const RESULTS_API_PATH = 'cms/v3/functions/results';
 
 async function getFunctionLogs(portalId, functionId, query = {}) {
-  const { limit = 5, after } = query;
+  const { limit = 5 } = query;
 
   return http.get(portalId, {
     uri: `${RESULTS_API_PATH}/${functionId}`,
-    query: {
-      limit,
-      after,
-    },
+    query: { ...query, limit },
   });
 }
 
