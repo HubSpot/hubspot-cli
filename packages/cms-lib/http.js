b473f669d167b88b1514dd985235b817efc8dd83
Anthony Pizzurro <apizzurro@hubspot.com>
2020-11-30 16:26:59 -0500

    handle ensureFile write errors

--- packages/cms-lib/http.js
+++ packages/cms-lib/http.js
@@ -150,8 +150,11 @@ const createGetRequestStream = ({ contentType }) => async (
       req.on('error', reject);
       req.on('response', res => {
         if (res.statusCode >= 200 && res.statusCode < 300) {
-          fs.ensureFileSync(filepath);
-
+          try {
+            fs.ensureFileSync(filepath);
+          } catch (err) {
+            reject(err);
+          }
           const writeStream = fs.createWriteStream(filepath, {
             encoding: 'binary',
           });
