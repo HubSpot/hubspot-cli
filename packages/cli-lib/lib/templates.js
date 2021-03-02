884d91123d5c1074ce239c93722917151e20e08f
brodgers <brodgers@hubspot.com>
2021-03-02 17:50:31 -0500

    delete unused file

--- packages/cli-lib/lib/templates.js
+++
@@ -1,8 +0,0 @@
-const { getExt } = require('./path');
-const { TEMPLATE_EXTENSION } = require('./lib/constants');
-
-function isTemplate(path) {
-  return getExt(path) === TEMPLATE_EXTENSION;
-}
-
-module.exports = { isTemplate };
