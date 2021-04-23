f470e7b5bb9fcfe85f4ffa138d45db6e4090d8a2
Mike Talley <mtalley@hubspot.com>
2021-04-23 12:32:09 -0400

    added basic error handling

--- packages/cli-lib/lib/lang.js
+++ packages/cli-lib/lib/lang.js
@@ -5,6 +5,8 @@ const handlebars = require('handlebars');
 const yaml = require('js-yaml');
 const { logger } = require('../logger');
 
+const MISSING_LANGUAGE_DATA_PREFIX = '[ Missing language data ]:';
+
 let languageObj;
 
 const loadLanguageFromYaml = () => {
@@ -25,11 +27,21 @@ const loadLanguageFromYaml = () => {
 
 const getTextValue = lookupDotNotation => {
   const lookupProps = lookupDotNotation.split('.');
+  const missingTextData = `${MISSING_LANGUAGE_DATA_PREFIX} ${lookupDotNotation}`;
   let textValue = languageObj;
 
-  lookupProps.forEach(prop => {
-    textValue = textValue[prop];
-  });
+  try {
+    lookupProps.forEach(prop => {
+      textValue = textValue[prop];
+    });
+  } catch (e) {
+    logger.error(`Unable to access language property: ${lookupDotNotation}`);
+    return missingTextData;
+  }
+
+  if (!textValue) {
+    return missingTextData;
+  }
 
   return textValue;
 };
@@ -42,8 +54,12 @@ const getInterpolatedValue = (textValue, options) => {
 
 const i18n = (lookupDotNotation, options = {}) => {
   const textValue = getTextValue(lookupDotNotation);
+  const shouldInterpolate =
+    options.data && !textValue.startsWith(MISSING_LANGUAGE_DATA_PREFIX);
 
-  return getInterpolatedValue(textValue, options.data);
+  return shouldInterpolate
+    ? getInterpolatedValue(textValue, options.data)
+    : textValue;
 };
 
 loadLanguageFromYaml();
