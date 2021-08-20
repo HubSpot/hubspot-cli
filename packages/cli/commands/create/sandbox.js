a6ec67992ec1bbd8790b7ce3a9b15fa9f4d142e2
Mike Talley <mtalley@hubspot.com>
2021-08-20 14:40:33 -0400

    added messaging to run auth after sandbox is created

--- packages/cli/commands/create/sandbox.js
+++ packages/cli/commands/create/sandbox.js
@@ -2,6 +2,7 @@ const { createSandbox } = require('@hubspot/cli-lib/sandboxes');
 const { loadAndValidateOptions } = require('../../lib/validation');
 const { createSandbox: prompt } = require('../../lib/prompts/sandboxes');
 const { getAccountId } = require('../../lib/commonOpts');
+const { logger } = require('../../../cli-lib/logger');
 
 module.exports = {
   dest: ({ name }) => name,
@@ -10,6 +11,15 @@ module.exports = {
     const accountId = getAccountId(options);
     const promptValues = await prompt();
     const sandbox = createSandbox(accountId, promptValues.name);
+
+    sandbox.then(({ name, sandboxHubId }) => {
+      logger.success(
+        `Sandbox "${name}" with portalId "${sandboxHubId}" created successfully.`
+      );
+      logger.info(
+        `Run "hs auth" to authenticate with the new sandbox account.`
+      );
+    });
 
     return sandbox;
   },
