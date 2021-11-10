8b10824c237f92ade0b25d8d930e8711aefb5fb2
Mike Talley <mtalley@hubspot.com>
2021-11-10 09:03:18 -0500

    added translations for hs filemanager

--- packages/cli/commands/filemanager.js
+++ packages/cli/commands/filemanager.js
@@ -5,9 +5,12 @@ const {
 } = require('../lib/commonOpts');
 const upload = require('./filemanager/upload');
 const fetch = require('./filemanager/fetch');
+const { i18n } = require('@hubspot/cli-lib/lib/lang');
+
+const i18nKey = 'cli.commands.filemanager';
 
 exports.command = 'filemanager';
-exports.describe = 'Commands for working with the File Manager';
+exports.describe = i18n(`${i18nKey}.describe`);
 
 exports.builder = yargs => {
   addOverwriteOptions(yargs, true);
