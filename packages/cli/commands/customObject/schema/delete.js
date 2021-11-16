0a1d70b2dabaaf7b67d2c872a8c3b845a5a9f744
Mike Talley <mtalley@hubspot.com>
2021-11-16 13:43:52 -0500

    fixed i18n reference key

--- packages/cli/commands/customObject/schema/delete.js
+++ packages/cli/commands/customObject/schema/delete.js
@@ -14,7 +14,7 @@ const { deleteSchema } = require('@hubspot/cli-lib/api/schema');
 const { i18n } = require('@hubspot/cli-lib/lib/lang');
 
 const i18nKey =
-  'cli.commands.customObject.subcommands.schema.subcommands.create';
+  'cli.commands.customObject.subcommands.schema.subcommands.delete';
 
 exports.command = 'delete <name>';
 exports.describe = i18n(`${i18nKey}.describe`);
