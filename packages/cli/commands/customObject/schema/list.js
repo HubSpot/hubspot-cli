909c9dc32185b98309effe6f533c1eb48ad55b08
Mike Talley <mtalley@hubspot.com>
2021-11-16 15:31:55 -0500

    added translations for hs custom-object list

--- packages/cli/commands/customObject/schema/list.js
+++ packages/cli/commands/customObject/schema/list.js
@@ -11,9 +11,12 @@ const { trackCommandUsage } = require('../../../lib/usageTracking');
 const { setLogLevel, getAccountId } = require('../../../lib/commonOpts');
 const { logDebugInfo } = require('../../../lib/debugInfo');
 const { listSchemas } = require('@hubspot/cli-lib/schema');
+const { i18n } = require('@hubspot/cli-lib/lib/lang');
+
+const i18nKey = 'cli.commands.customObject.subcommands.schema.subcommands.list';
 
 exports.command = 'list';
-exports.describe = 'List schemas available on your account';
+exports.describe = i18n(`${i18nKey}.describe`);
 
 exports.handler = async options => {
   setLogLevel(options);
@@ -32,6 +35,6 @@ exports.handler = async options => {
     await listSchemas(accountId);
   } catch (e) {
     logErrorInstance(e);
-    logger.error(`Unable to list schemas`);
+    logger.error(i18n(`${i18nKey}.errors.list`));
   }
 };
