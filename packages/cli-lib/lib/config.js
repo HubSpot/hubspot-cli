8d078561a4581570f8e2e6a71e93e29e70cd104e
Anthony Pizzurro <anthony@anthmatic.com>
2021-11-16 16:34:11 -0500

    move ui utils to their own file

--- packages/cli-lib/lib/config.js
+++ packages/cli-lib/lib/config.js
@@ -415,16 +415,6 @@ const getAccountConfig = accountId =>
     account => account.portalId === accountId
   );
 
-/**
- * Obtains formatted account description from config
- */
-const getAccountDescription = accountId => {
-  const account = getAccountConfig(accountId);
-  return account.name
-    ? `${account.name} (${account.portalId})`
-    : account.portalId;
-};
-
 /*
  * Returns a portalId from the config if it exists, else returns null
  */
@@ -816,7 +806,6 @@ module.exports = {
   findConfig,
   loadConfigFromEnvironment,
   getAccountConfig,
-  getAccountDescription,
   getAccountId,
   updateAccountConfig,
   updateDefaultAccount,
