3aad4ba841bd0ea1460d69410e8dd70f00032494
Mike Talley <mtalley@hubspot.com>
2021-12-01 16:17:03 -0500

    added more comments

--- packages/cli-lib/lib/interpolationHelpers.js
+++ packages/cli-lib/lib/interpolationHelpers.js
@@ -1,5 +1,9 @@
 const chalk = require('chalk');
 
+/**
+ * These helper methods are used to modify text output within the CLI. They
+ * should all take in a string value and output a modified string value.a
+ */
 module.exports = {
   bold: function(stringValue) {
     return chalk.bold(stringValue);
