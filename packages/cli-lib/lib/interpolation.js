83d3c93534ac874053c24756aa13aae1675cb847
Mike Talley <mtalley@hubspot.com>
2021-12-01 16:14:51 -0500

    added comments

--- packages/cli-lib/lib/interpolation.js
+++ packages/cli-lib/lib/interpolation.js
@@ -25,6 +25,15 @@ const generateReplaceFn = (matchedText, startIndex, replacementString) => {
   };
 };
 
+/**
+ * Interpolate a string with data
+ * @param {string} stringValue - The string to interpolate
+ * @param {object} interpolationData - The data to interpolate with
+ * @returns {string} - The interpolated string
+ * @example
+ * interpolation('Hello {{name}}', { name: 'World' })
+ * // 'Hello World'
+ */
 const interpolation = (stringValue, interpolationData) => {
   const interpolationIdentifierRegEx = new RegExp(
     `${delimiters.interpolation.start}(.*?)${delimiters.interpolation.end}`,
@@ -33,6 +42,8 @@ const interpolation = (stringValue, interpolationData) => {
   const replaceQueue = [];
   let match;
 
+  // while & reduce necessary because RegExp.exec is stateful and only runs
+  // from beginning to end of string
   while ((match = interpolationIdentifierRegEx.exec(stringValue)) != null) {
     const { 0: matchedText, 1: rawIdentifier, index } = match;
     const identifier = rawIdentifier.trim();
@@ -52,6 +63,16 @@ const interpolation = (stringValue, interpolationData) => {
   return compiledString;
 };
 
+/**
+ * Compile a string using a specified helper function
+ * @param {string} stringValue - The string to modify
+ * @param {object} helperIdentifier - Helper name
+ * @param {function} helperFn - Helper function to call on string
+ * @returns {string} - The modified string
+ * @example
+ * compileHelper('White {{#yellow}}yellow{{/yellow}}', 'yellow', (string) => { chalk.reset.yellow(string) }))))
+ * // 'White yellow' (with 'yellow' colored yellow)
+ */
 const compileHelper = (stringValue, helperIdentifier, helperFn) => {
   const helperIdentifierRegEx = new RegExp(
     `${delimiters.interpolation.start}(${delimiters.helpers.start}${helperIdentifier})${delimiters.interpolation.end}(.*?)${delimiters.interpolation.start}(${delimiters.helpers.end}${helperIdentifier})${delimiters.interpolation.end}`,
@@ -60,6 +81,8 @@ const compileHelper = (stringValue, helperIdentifier, helperFn) => {
   const replaceQueue = [];
   let match;
 
+  // while & reduce necessary because RegExp.exec is stateful and only runs
+  // from beginning to end of string
   while ((match = helperIdentifierRegEx.exec(stringValue)) != null) {
     const {
       0: matchedText,
@@ -96,6 +119,15 @@ const compileHelpers = stringValue => {
   }, stringValue);
 };
 
+/**
+ * Interpolate a string with data and compile helpers on the string
+ * @param {string} stringValue - The string to interpolate
+ * @param {object} interpolationData - The data to interpolate with
+ * @returns {string} - The interpolated and helper-compiled string
+ * @example
+ * interpolateAndCompile('Some {{#bold}}{{text}}{{/bold}} text', { text: 'awesomely bold' })
+ * // 'Some awssomely bold text' (with the words 'awesomely bold' in bold)
+ */
 const interpolate = (stringValue, interpolationData) => {
   const interpolatedString = interpolation(stringValue, interpolationData);
   const helperCompiledString = compileHelpers(interpolatedString);
