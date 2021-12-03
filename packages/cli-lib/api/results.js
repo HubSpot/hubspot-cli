9f0d52c7bc7562c4735d76febd22466d975fb4bb
Anthony Pizzurro <anthony@anthmatic.com>
2021-12-03 14:17:07 -0500

    encode project name in request url

--- packages/cli-lib/api/results.js
+++ packages/cli-lib/api/results.js
@@ -6,14 +6,14 @@ async function getFunctionLogs(accountId, route, query = {}) {
   const { limit = 5 } = query;
 
   return http.get(accountId, {
-    uri: `${RESULTS_API_PATH}/by-route/${route}`,
+    uri: `${RESULTS_API_PATH}/by-route/${encodeURIComponent(route)}`,
     query: { ...query, limit },
   });
 }
 
 async function getLatestFunctionLog(accountId, route) {
   return http.get(accountId, {
-    uri: `${RESULTS_API_PATH}/by-route/${route}/latest`,
+    uri: `${RESULTS_API_PATH}/by-route/${encodeURIComponent(route)}/latest`,
   });
 }
 
