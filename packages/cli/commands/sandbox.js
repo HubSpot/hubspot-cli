345ae52c449afb554ada84bb1ef2a13f404ab9a9
Adam Wang <awang@hubspot.com>
2022-04-13 16:54:42 -0400

    Add command descriptions, add translation

--- packages/cli/commands/sandbox.js
+++ packages/cli/commands/sandbox.js
@@ -1,8 +1,11 @@
+const { i18n } = require('@hubspot/cli-lib/lib/lang');
 const { addConfigOptions, addAccountOptions } = require('../lib/commonOpts');
 const create = require('./sandbox/create');
 
+const i18nKey = 'cli.commands.sandbox';
+
 exports.command = 'sandbox';
-exports.describe = false; //'Commands for working with sandboxes';
+exports.describe = i18n(`${i18nKey}.describe`);
 
 exports.builder = yargs => {
   addConfigOptions(yargs, true);
