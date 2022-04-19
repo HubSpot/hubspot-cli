1a4e8fdde75131a234d0eb8b4a8468c2e8e6cc40
Jessica Sines <jsines99@gmail.com>
2022-04-19 16:54:49 -0400

    Hide module command from help menu while testing internally

--- packages/cli/commands/module.js
+++ packages/cli/commands/module.js
@@ -1,11 +1,11 @@
 const marketplaceValidate = require('./module/marketplace-validate');
 const { addConfigOptions, addAccountOptions } = require('../lib/commonOpts');
-const { i18n } = require('@hubspot/cli-lib/lib/lang');
+// const { i18n } = require('@hubspot/cli-lib/lib/lang');
 
-const i18nKey = 'cli.commands.module';
+// const i18nKey = 'cli.commands.module';
 
 exports.command = 'module';
-exports.describe = i18n(`${i18nKey}.describe`);
+exports.describe = false; //i18n(`${i18nKey}.describe`);
 
 exports.builder = yargs => {
   addConfigOptions(yargs, true);
