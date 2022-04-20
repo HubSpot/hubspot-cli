480ef7b4499f8761eb0ee73c06acdb0d432b1217
Branden Rodgers <brodgers@hubspot.com>
2022-04-20 16:35:04 -0400

    cleaning up unnecessary changes

--- packages/cli-lib/lib/read.js
+++ packages/cli-lib/lib/read.js
@@ -30,7 +30,6 @@ const getFileInfoAsync = (dir, file) => {
 };
 
 const flattenAndRemoveSymlinks = filesData => {
-  console.log('filesData :>> ', filesData);
   return filesData.reduce((acc, fileData) => {
     switch (fileData.type) {
       case STAT_TYPES.FILE:
@@ -46,13 +45,13 @@ const flattenAndRemoveSymlinks = filesData => {
 };
 
 async function read(dir) {
-  const processFiles = files => {
-    return Promise.all(files.map(file => getFileInfoAsync(dir, file)));
-  };
+  const processFiles = files =>
+    Promise.all(files.map(file => getFileInfoAsync(dir, file)));
 
   return fs.promises
     .readdir(dir)
     .then(processFiles)
+    .then(flattenAndRemoveSymlinks)
     .catch(err => {
       logger.debug(err);
       return [];
