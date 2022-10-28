12fd38598d125727bd5b6aa9a3ee2d0b2b8946c1
Allison Kemmerle <akemmerle@hubspot.com>
2022-10-28 13:13:54 -0400

    Remove unnecessary asyncs

--- packages/cli-lib/api/marketplaceValidation.js
+++ packages/cli-lib/api/marketplaceValidation.js
@@ -3,11 +3,10 @@ const http = require('../http');
 const VALIDATION_API_BASE = 'quality-engine/v1/validation';
 
 /**
- * @async
  * @param {number} accountId
  * @returns {Promise}
  */
-async function requestValidation(accountId, body = {}) {
+function requestValidation(accountId, body = {}) {
   return http.post(accountId, {
     uri: `${VALIDATION_API_BASE}/request`,
     body,
@@ -15,11 +14,10 @@ async function requestValidation(accountId, body = {}) {
 }
 
 /**
- * @async
  * @param {number} accountId
  * @returns {Promise}
  */
-async function getValidationStatus(accountId, query = {}) {
+function getValidationStatus(accountId, query = {}) {
   return http.get(accountId, {
     uri: `${VALIDATION_API_BASE}/status`,
     query,
@@ -27,11 +25,10 @@ async function getValidationStatus(accountId, query = {}) {
 }
 
 /**
- * @async
  * @param {number} accountId
  * @returns {Promise}
  */
-async function getValidationResults(accountId, query = {}) {
+function getValidationResults(accountId, query = {}) {
   return http.get(accountId, {
     uri: `${VALIDATION_API_BASE}/results`,
     query,
