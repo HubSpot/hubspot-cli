8672479ec0dc4fba59358927c47e34b5b6cfba28
Allison Kemmerle <akemmerle@hubspot.com>
2023-05-30 12:05:33 -0400

    Change repoPath flag to templateSource

--- packages/cli/commands/project/create.js
+++ packages/cli/commands/project/create.js
@@ -35,7 +35,7 @@ exports.handler = async options => {
     path.resolve(getCwd(), options.location || location),
     options.name || name,
     options.template || template,
-    options.repoPath
+    options.templateSource
   );
 
   logger.log('');
@@ -62,8 +62,8 @@ exports.builder = yargs => {
       describe: i18n(`${i18nKey}.options.template.describe`),
       type: 'string',
     },
-    repoPath: {
-      describe: i18n(`${i18nKey}.options.repoPath.describe`),
+    templateSource: {
+      describe: i18n(`${i18nKey}.options.templateSource.describe`),
       type: 'string',
     },
   });
