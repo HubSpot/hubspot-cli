0c65e5df3e815b8ab93eb149ae4d19f864e05400
Adam Wang <awang@hubspot.com>
2023-07-26 12:25:35 -0400

    Confirmation for marked portals, update message copy, spinnies

--- packages/cli/commands/accounts/clean.js
+++ packages/cli/commands/accounts/clean.js
@@ -1,9 +1,8 @@
 const { logger } = require('@hubspot/cli-lib/logger');
 const {
-  // accessTokenForPersonalAccessKey,
-  getAccessToken,
+  accessTokenForPersonalAccessKey,
 } = require('@hubspot/cli-lib/personalAccessKey');
-const { getConfig, getAccountConfig } = require('@hubspot/cli-lib');
+const { getConfig } = require('@hubspot/cli-lib');
 
 const { trackCommandUsage } = require('../../lib/usageTracking');
 const { i18n } = require('../../lib/lang');
@@ -17,6 +16,10 @@ const {
 } = require('../../lib/commonOpts');
 const { HubSpotAuthError } = require('@hubspot/cli-lib/lib/models/Errors');
 const { getAccountName } = require('../../lib/sandboxes');
+const { promptUser } = require('../../lib/prompts/promptUtils');
+const { getTableContents } = require('@hubspot/cli-lib/lib/table');
+const SpinniesManager = require('../../lib/SpinniesManager');
+const { deleteAccount } = require('@hubspot/cli-lib/lib/config');
 
 const i18nKey = 'cli.commands.accounts.subcommands.clean';
 
@@ -35,35 +38,73 @@ exports.handler = async options => {
     qa ? p.env === 'qa' : p.env !== 'qa'
   );
 
+  if (filteredTestPortals && filteredTestPortals.length === 0) {
+    logger.log(i18n(`${i18nKey}.noResults`));
+    process.exit(EXIT_CODES.SUCCESS);
+  }
+
+  const portalsToRemove = [];
+  SpinniesManager.init({
+    succeedColor: 'white',
+  });
+  SpinniesManager.add('accountsClean', {
+    text: i18n(`${i18nKey}.loading.add`),
+  });
+
   for (const account of filteredTestPortals) {
-    console.log('account here: ', getAccountName(account));
     try {
-      // await accessTokenForPersonalAccessKey(account.portalId);
-      const { personalAccessKey, env } = getAccountConfig(account.portalId);
-      await getAccessToken(personalAccessKey, env, account.portalId);
+      await accessTokenForPersonalAccessKey(account.portalId);
     } catch (error) {
       if (error && error instanceof HubSpotAuthError) {
         if (
           error.statusCode === 404 &&
           error.subCategory === 'LocalDevAuthErrorType.INVALID_PORTAL_ID'
         ) {
-          // TODO: clean up portal here
-          console.log('Invalid portal error here: ', error);
-          logger.log(
-            i18n(`${i18nKey}.inactivePortalFound`, {
-              accountName: getAccountName(account),
-            })
-          );
-          // remove account here
-        } else {
-          logger.log(`Skipping ${getAccountName(account)}`, error);
+          portalsToRemove.push(account);
         }
-      } else {
-        logger.log(`Skipping ${getAccountName(account)}`);
       }
     }
   }
+  SpinniesManager.succeed('accountsClean', {
+    text: i18n(`${i18nKey}.loading.succeed`),
+  });
 
+  logger.log('');
+  if (portalsToRemove.length > 0) {
+    logger.log(
+      i18n(`${i18nKey}.portalsMarkedForRemoval`, {
+        count: portalsToRemove.length,
+      })
+    );
+    logger.log(
+      getTableContents(
+        portalsToRemove.map(p => [getAccountName(p)]),
+        { border: { bodyLeft: '  ' } }
+      )
+    );
+    const { accountsCleanPrompt } = await promptUser([
+      {
+        name: 'accountsCleanPrompt',
+        type: 'confirm',
+        message: i18n(`${i18nKey}.confirm`),
+      },
+    ]);
+    if (accountsCleanPrompt) {
+      logger.log('');
+      for (const accountToRemove of portalsToRemove) {
+        await deleteAccount(accountToRemove);
+        logger.log(
+          i18n(`${i18nKey}.removeSuccess`, {
+            accountName: getAccountName(accountToRemove),
+          })
+        );
+      }
+    }
+  } else {
+    logger.log(i18n(`${i18nKey}.noResults`));
+  }
+
+  logger.log('');
   process.exit(EXIT_CODES.SUCCESS);
 };
 
