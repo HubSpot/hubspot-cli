5f2e45fdff3c56b4187aee9fd3068dd23963f580
Branden Rodgers <brodgers@hubspot.com>
2023-08-15 16:52:14 -0400

    update project create next steps. remove deprecated ui beta warnign util

--- packages/cli/commands/customObject.js
+++ packages/cli/commands/customObject.js
@@ -1,10 +1,9 @@
-const chalk = require('chalk');
 const { addConfigOptions, addAccountOptions } = require('../lib/commonOpts');
 const schemaCommand = require('./customObject/schema');
 const createCommand = require('./customObject/create');
 const { i18n } = require('../lib/lang');
 const { logger } = require('@hubspot/cli-lib/logger');
-const { uiBetaWarning } = require('../lib/ui');
+const { uiBetaMessage, uiLink } = require('../lib/ui');
 
 const i18nKey = 'cli.commands.customObject';
 
@@ -12,17 +11,14 @@ exports.command = ['custom-object', 'custom', 'co'];
 exports.describe = i18n(`${i18nKey}.describe`);
 
 const logBetaMessage = () => {
-  uiBetaWarning(() => {
-    logger.log(chalk.reset.yellow(i18n(`${i18nKey}.betaMessage`)));
-    logger.log(
-      chalk.reset.yellow(
-        i18n(`${i18nKey}.seeMoreLink`, {
-          link:
-            'https://developers.hubspot.com/docs/api/crm/crm-custom-objects',
-        })
-      )
-    );
-  });
+  uiBetaMessage(i18n(`${i18nKey}.betaMessage`));
+  logger.log(
+    uiLink(
+      i18n(`${i18nKey}.seeMoreLink`),
+      'https://developers.hubspot.com/docs/api/crm/crm-custom-objects'
+    )
+  );
+  logger.log();
 };
 
 exports.builder = yargs => {
