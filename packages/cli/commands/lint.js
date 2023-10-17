bb35cfd104001b1795f12647b20895b571efa23b
Camden Phalen <cphalen@hubspot.com>
2023-10-17 15:34:38 -0400

    revert validate changes

--- packages/cli/commands/lint.js
+++ packages/cli/commands/lint.js
@@ -1,7 +1,7 @@
-const { lint } = require('@hubspot/local-dev-lib/validate');
+const { lint } = require('@hubspot/cli-lib/validate');
 const { printHublValidationResult } = require('../lib/hublValidate');
 const { logger } = require('@hubspot/cli-lib/logger');
-const { logErrorInstance } = require('../lib/errorHandlers/standardErrors');
+const { logErrorInstance } = require('@hubspot/cli-lib/errorHandlers');
 
 const {
   addConfigOptions,
