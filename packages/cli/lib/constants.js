88883fdaeecda0b7d82647f5ad84cfb6b4987351
Camden Phalen <cphalen@hubspot.com>
2024-03-06 13:46:42 -0500

    Start swapping constant deps

--- packages/cli/lib/constants.js
+++ packages/cli/lib/constants.js
@@ -2,7 +2,21 @@ const HUBSPOT_PROJECT_COMPONENTS_GITHUB_PATH =
   'HubSpot/hubspot-project-components';
 const DEFAULT_PROJECT_TEMPLATE_BRANCH = 'main';
 
+const FEEDBACK_OPTIONS = {
+  BUG: 'bug',
+  GENERAL: 'general',
+};
+const FEEDBACK_URLS = {
+  BUG: 'https://github.com/HubSpot/hubspot-cli/issues/new',
+  GENERAL:
+    'https://docs.google.com/forms/d/e/1FAIpQLSejZZewYzuH3oKBU01tseX-cSWOUsTHLTr-YsiMGpzwcvgIMg/viewform?usp=sf_link',
+};
+const FEEDBACK_INTERVAL = 10;
+
 module.exports = {
   HUBSPOT_PROJECT_COMPONENTS_GITHUB_PATH,
   DEFAULT_PROJECT_TEMPLATE_BRANCH,
+  FEEDBACK_OPTIONS,
+  FEEDBACK_URLS,
+  FEEDBACK_INTERVAL,
 };
