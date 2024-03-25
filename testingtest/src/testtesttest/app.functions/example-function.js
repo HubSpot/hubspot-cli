ed76a433a518a06943bce4faa82e507efced4cbb
Adam Wang <awang@hubspot.com>
2024-03-25 16:16:52 -0400

    remove test data

--- testingtest/src/testtesttest/app.functions/example-function.js
+++
@@ -1,7 +0,0 @@
-exports.main = async (context = {}) => {
-  const { text } = context.parameters;
-
-  const response = `This is coming from a serverless function! You entered: ${text}`;
-
-  return response;
-};
