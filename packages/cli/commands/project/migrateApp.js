e3b79d605256b75235f26f58a4bdb90f02aae6f5
Allison Kemmerle <akemmerle@hubspot.com>
2024-06-06 14:25:44 -0400

    Implement option 1

--- packages/cli/commands/project/migrateApp.js
+++ packages/cli/commands/project/migrateApp.js
@@ -80,21 +80,9 @@ exports.handler = async options => {
         });
 
   const publicApps = await fetchPublicAppOptions(accountId, accountName);
-  const selectedApp = publicApps.find(a => a.id === appId);
-  if (!selectedApp) {
+  if (!publicApps.find(a => a.id === appId)) {
     logger.error(i18n(`${i18nKey}.errors.invalidAppId`, { appId }));
     process.exit(EXIT_CODES.ERROR);
-  }
-  if (selectedApp.listingInfo) {
-    const { shouldContinue } = await promptUser({
-      name: 'shouldContinue',
-      type: 'confirm',
-      message: i18n(`${i18nKey}.continuePrompt`),
-    });
-
-    if (!shouldContinue) {
-      process.exit(EXIT_CODES.SUCCESS);
-    }
   }
 
   const { name, location } = await createProjectPrompt('', options, true);
