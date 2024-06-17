1dcfee95544dad988968e71fb816299a01c6a82f
Allison Kemmerle <akemmerle@hubspot.com>
2024-06-17 13:28:48 -0400

    Move functions to local-dev-lib & add location validation

--- packages/cli/lib/prompts/createProjectPrompt.js
+++ packages/cli/lib/prompts/createProjectPrompt.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const { getCwd } = require('@hubspot/local-dev-lib/path');
+const {
+  getCwd,
+  sanitizeFileName,
+  isValidPath,
+} = require('@hubspot/local-dev-lib/path');
 const { PROJECT_COMPONENT_TYPES } = require('../../lib/constants');
 const { promptUser } = require('./promptUtils');
 const { fetchFileFromRepository } = require('@hubspot/local-dev-lib/github');
@@ -15,27 +19,6 @@ const {
 const i18nKey = 'lib.prompts.createProjectPrompt';
 
 const PROJECT_PROPERTIES = ['name', 'label', 'path', 'insertPath'];
-
-// Based on the node-sanitize-filename package: https://github.com/parshap/node-sanitize-filename/blob/master/index.js
-const sanitizeProjectName = projectName => {
-  // Windows invalid/control characters
-  // eslint-disable-next-line no-control-regex
-  const invalidChars = /[<>:"/|?*\x00-\x1F]/g;
-
-  //Replace invalid characters with dash
-  let sanitizedProjectName = projectName.replace(invalidChars, '-');
-
-  // Removes trailing periods and spaces for Windows
-  sanitizedProjectName = sanitizedProjectName.replace(/[. ]+$/, '');
-
-  // Reserved names check (Windows specific)
-  const reservedNames = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
-  if (reservedNames.test(sanitizedProjectName)) {
-    sanitizedProjectName = `-${sanitizedProjectName}`;
-  }
-
-  return sanitizedProjectName;
-};
 
 const hasAllProperties = projectList => {
   return projectList.every(config =>
@@ -100,7 +83,7 @@ const createProjectPrompt = async (
       message: i18n(`${i18nKey}.enterLocation`),
       when: !promptOptions.location,
       default: answers => {
-        const projectName = sanitizeProjectName(
+        const projectName = sanitizeFileName(
           answers.name || promptOptions.name
         );
         return path.resolve(getCwd(), projectName);
@@ -111,6 +94,9 @@ const createProjectPrompt = async (
         }
         if (fs.existsSync(input)) {
           return i18n(`${i18nKey}.errors.invalidLocation`);
+        }
+        if (!isValidPath(input)) {
+          return i18n(`${i18nKey}.errors.invalidCharacters`);
         }
         return true;
       },
