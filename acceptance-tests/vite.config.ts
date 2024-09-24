6b17f53b32c4e7168d45678e9bb58da284f2c2ba
Joe Yeager <jyeager@hubspot.com>
2024-09-24 16:39:05 -0700

    More cleanup

--- acceptance-tests/vite.config.ts
+++ acceptance-tests/vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     setupFiles: ['./setup'],
     include: ['./tests/**/*.spec.ts'],
     exclude: ['./lib/**/*'],
+    // Don't run the test files in parallel.
+    // To accomplish this, we need to refactor the tests so there aren't filesystem collisions
     fileParallelism: false,
   },
 });
