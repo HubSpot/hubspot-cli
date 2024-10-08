cb7ef29b0064e2a977166234f57687027b94aa97
Joe Yeager <jyeager@hubspot.com>
2024-10-08 13:46:58 -0700

    Add docker step

--- acceptance-tests/globalSetup.ts
+++ acceptance-tests/globalSetup.ts
@@ -1,14 +1,23 @@
 import { existsSync, mkdirSync, rmSync } from 'fs';
 import { testOutputDir } from './lib/testState';
 import type { GlobalSetupContext } from 'vitest/node'
+import * as dotEnv from "dotenv";
+import * as path from "path";
+
 
 // Vitest docs on globalSetup modules https://vitest.dev/config/#globalsetup
 export function setup(__context: GlobalSetupContext) {
+    const dotEnvConfig = dotEnv.config({ path: path.join(__dirname, './.env') });
+    const { parsed }  = dotEnvConfig
+    if(parsed && parsed.USE_INSTALLED) {
+        console.log('Using installed version of the hs command')
+    }
     try {
         if(existsSync(testOutputDir)) {
-            console.log(`${testOutputDir} already exists, deleting it's contents\n`);
+            console.log(`The directory ${testOutputDir} already exists, deleting it's contents\n`);
             rmSync(testOutputDir, { recursive: true, force: true });
         }
+
         console.log(`Setting up ${testOutputDir} directory\n`);
         mkdirSync(testOutputDir);
     } catch (e) {
