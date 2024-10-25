a9b4f80f6fd75d6c357225793593d3f43bb7e9ad
Camden Phalen <cphalen@hubspot.com>
2024-10-25 12:01:15 -0400

    convert lib/interpolationHelpers to TS

--- lib/interpolationHelpers.ts
+++ lib/interpolationHelpers.ts
@@ -1,27 +1,29 @@
-// @ts-nocheck
-const chalk = require('chalk');
+import chalk from 'chalk';
 
 /**
  * These helper methods are used to modify text output within the CLI. They
  * should all take in a string value and output a modified string value.
  */
-module.exports = {
-  bold: function(stringValue) {
-    return chalk.bold(stringValue);
-  },
-  yellow: function(stringValue) {
-    return chalk.reset.yellow(stringValue);
-  },
-  green: function(stringValue) {
-    return chalk.reset.green(stringValue);
-  },
-  red: function(stringValue) {
-    return chalk.reset.red(stringValue);
-  },
-  cyan: function(stringValue) {
-    return chalk.cyan(stringValue);
-  },
-  orange: function(stringValue) {
-    return chalk.hex('#FC9900')(stringValue);
-  },
-};
+
+export function bold(stringValue: string): string {
+  return chalk.bold(stringValue);
+}
+
+export function yellow(stringValue: string): string {
+  return chalk.reset.yellow(stringValue);
+}
+
+export function green(stringValue: string): string {
+  return chalk.reset.green(stringValue);
+}
+
+export function red(stringValue: string): string {
+  return chalk.reset.red(stringValue);
+}
+
+export function cyan(stringValue: string): string {
+  return chalk.cyan(stringValue);
+}
+export function orange(stringValue: string): string {
+  return chalk.hex('#FC9900')(stringValue);
+}
