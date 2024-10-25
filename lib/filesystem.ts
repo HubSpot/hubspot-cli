8b17fd1bbda0b281d7f7a24d48198a22d1c9191f
Camden Phalen <cphalen@hubspot.com>
2024-10-25 11:40:20 -0400

    convert lib/filesystem to TS

--- lib/filesystem.ts
+++ lib/filesystem.ts
@@ -1,18 +1,15 @@
-// @ts-nocheck
-const path = require('path');
-const { getCwd } = require('@hubspot/local-dev-lib/path');
-const {
-  FOLDER_DOT_EXTENSIONS,
-} = require('@hubspot/local-dev-lib/constants/extensions');
+import path from 'path';
+import { getCwd } from '@hubspot/local-dev-lib/path';
+import { FOLDER_DOT_EXTENSIONS } from '@hubspot/local-dev-lib/constants/extensions';
 
-function resolveLocalPath(filepath) {
+export function resolveLocalPath(filepath: string): string {
   return filepath && typeof filepath === 'string'
     ? path.resolve(getCwd(), filepath)
     : // Use CWD if optional filepath is not passed.
       getCwd();
 }
 
-function isPathFolder(path) {
+export function isPathFolder(path: string): boolean {
   const splitPath = path.split('/');
   const fileOrFolderName = splitPath[splitPath.length - 1];
   const splitName = fileOrFolderName.split('.');
@@ -26,8 +23,3 @@ function isPathFolder(path) {
 
   return true;
 }
-
-module.exports = {
-  resolveLocalPath,
-  isPathFolder,
-};
