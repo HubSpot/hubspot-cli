4520abcfad915a0bf9dfeb536a24ff8c64943a58
Camden Phalen <cphalen@hubspot.com>
2024-10-30 16:44:15 -0400

    fetch dist tags

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,16 +1,30 @@
 import { exec } from 'child_process';
-import yargs, { Arguments, Argv } from 'yargs';
+import { name as packageName } from '../package.json';
+import yargs, { ArgumentsCamelCase, Argv } from 'yargs';
 import { logger } from '@hubspot/local-dev-lib/logger';
 
 import { EXIT_CODES } from '../lib/enums/exitCodes';
 
 const MAIN_BRANCH = 'improved-release-script';
+const VERSION_INCREMENTS = ['patch', 'minor', 'major'] as const;
+const TAGS = ['latest', 'next', 'experimental'] as const;
+
+type ReleaseArguments = {
+  versionIncrement: typeof VERSION_INCREMENTS[number];
+  tag: typeof TAGS[number];
+};
+
+type DistTags = {
+  latest: string;
+  next: string;
+  experimental: string;
+};
 
 function isMainBranch(): Promise<boolean> {
   return new Promise(resolve => {
     exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
       if (error) {
-        logger.error('An error occured while checking the branch');
+        logger.error(`An error occured while checking the branch: ${error}`);
         process.exit(EXIT_CODES.ERROR);
       }
 
@@ -20,28 +34,51 @@ function isMainBranch(): Promise<boolean> {
   });
 }
 
-async function handler({ versionIncrement, tag }: Arguments): Promise<void> {
+function getDistTags(): Promise<DistTags> {
+  return new Promise(resolve => {
+    exec(`npm view ${packageName} dist-tags --json`, async (error, stdout) => {
+      if (error) {
+        logger.error(
+          `AN error occured while fetching current dist tags: ${error}`
+        );
+        process.exit(EXIT_CODES.ERROR);
+      }
+
+      const distTags = stdout.trim();
+      const json = await JSON.parse(distTags);
+      resolve(json);
+    });
+  });
+}
+
+async function handler({
+  versionIncrement,
+  tag,
+}: ArgumentsCamelCase<ReleaseArguments>): Promise<void> {
   const onCorrectBranch = await isMainBranch();
 
   if (!onCorrectBranch) {
     logger.error('New release can only be published on main branch');
     process.exit(EXIT_CODES.ERROR);
   }
+
+  const distTags = await getDistTags();
+  console.log(distTags);
 }
 
-async function builder(yargs: Argv) {
-  yargs.options({
+async function builder(yargs: Argv): Promise<Argv> {
+  return yargs.options({
     versionIncrement: {
       alias: 'v',
-      requiresArg: true,
+      demandOption: true,
       describe: 'SemVer increment type for the next release',
-      choices: ['patch', 'minor', 'major'] as const,
+      choices: VERSION_INCREMENTS,
     },
     tag: {
       alias: 't',
-      requiresArg: true,
+      demandOption: true,
       describe: 'Tag for the next release',
-      choices: ['latest', 'next', 'experimental'] as const,
+      choices: TAGS,
     },
   });
 }
