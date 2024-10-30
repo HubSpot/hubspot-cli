c47b360a45b931a755fdeb80d0d274913bb13667
Camden Phalen <cphalen@hubspot.com>
2024-10-30 13:07:35 -0400

    release script wip

--- scripts/release.ts
+++ scripts/release.ts
@@ -4,7 +4,7 @@ import { logger } from '@hubspot/local-dev-lib/logger';
 
 import { EXIT_CODES } from '../lib/enums/exitCodes';
 
-const MAIN_BRANCH = 'main';
+const MAIN_BRANCH = 'improved-release-script';
 
 function isMainBranch(): Promise<boolean> {
   return new Promise(resolve => {
@@ -20,34 +20,34 @@ function isMainBranch(): Promise<boolean> {
   });
 }
 
-async function handler({ version, tag }: Arguments): Promise<void> {
+async function handler({ versionIncrement, tag }: Arguments): Promise<void> {
   const onCorrectBranch = await isMainBranch();
 
   if (!onCorrectBranch) {
     logger.error('New release can only be published on main branch');
     process.exit(EXIT_CODES.ERROR);
   }
-
-  console.log('handler');
 }
 
 async function builder(yargs: Argv) {
-  yargs.option('versionIncrement', {
-    alias: 'v',
-    requiresArg: true,
-    describe: 'SemVer increment type for the next release',
-    choices: ['patch', 'minor', 'major'] as const,
+  yargs.options({
+    versionIncrement: {
+      alias: 'v',
+      requiresArg: true,
+      describe: 'SemVer increment type for the next release',
+      choices: ['patch', 'minor', 'major'] as const,
+    },
+    tag: {
+      alias: 't',
+      requiresArg: true,
+      describe: 'Tag for the next release',
+      choices: ['latest', 'next', 'experimental'] as const,
+    },
   });
-  yargs.option('tag', {
-    alias: 't',
-    requiresArg: true,
-    describe: 'Tag for the next release',
-    choices: ['latest', 'next', 'experimental'] as const,
-  });
-  // yargs.usage('HubSpot CLI release script');
 }
 
-const argv = yargs(process.argv.slice(2))
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+yargs(process.argv.slice(2))
   .scriptName('yarn')
   .usage('Hubspot CLI release script')
   .command(
