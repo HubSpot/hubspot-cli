e093f5560cc2a0dc2d8a5a784f6a399a7d1b7bef
Allison Kemmerle <akemmerle@hubspot.com>
2024-11-05 13:36:51 -0500

    Address feedback p1

--- lib/debugInfo.ts
+++ lib/debugInfo.ts
@@ -2,11 +2,7 @@ import { version } from '../package.json';
 import { getPlatform } from './environment';
 import { Arguments } from 'yargs';
 
-interface CustomOptions {
-  debug?: boolean;
-}
-
-export function logDebugInfo({ debug }: Arguments<CustomOptions>): void {
+export function logDebugInfo({ debug }: Arguments<{ debug?: boolean }>): void {
   if (!debug) {
     return;
   }
