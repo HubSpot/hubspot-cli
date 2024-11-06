863542d7962528ef620195d8993138c2c9f9b415
Camden Phalen <camdenphalen@gmail.com>
2024-11-06 12:31:42 -0500

    Improved release script (#1228)

---
+++ scripts/build.ts
@@ -0,0 +1,31 @@
+import { spawn } from 'child_process';
+import fs from 'fs';
+import { EXIT_CODES } from '../lib/enums/exitCodes';
+
+export async function build(): Promise<void> {
+  // Remove the current dist dir
+  fs.rmSync('dist', { recursive: true, force: true });
+
+  // Build typescript
+  await new Promise(resolve => {
+    const childProcess = spawn('yarn', ['tsc'], {
+      stdio: 'inherit',
+    });
+
+    childProcess.on('close', code => {
+      if (code !== EXIT_CODES.SUCCESS) {
+        process.exit(code);
+      }
+      resolve('');
+    });
+  });
+
+  // Copy remaining files
+  fs.cpSync('lang', 'dist/lang', { recursive: true });
+  fs.cpSync('bin/hs', 'dist/bin/hs');
+  fs.cpSync('bin/hscms', 'dist/bin/hscms');
+  fs.cpSync('README.md', 'dist/README.md');
+  fs.cpSync('LICENSE', 'dist/LICENSE');
+}
+
+build();
