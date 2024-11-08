571b4f72d40f4aebe7286e7c47bf86500c3b835e
Allison Kemmerle <akemmerle@hubspot.com>
2024-11-08 11:50:19 -0500

    Remove unnecessary cast

--- lib/ui/SpinniesManager.ts
+++ lib/ui/SpinniesManager.ts
@@ -18,7 +18,6 @@ import {
   getLinesLength,
   purgeSpinnerOptions,
   purgeSpinnersOptions,
-  Spinner,
   SPINNERS,
   terminalSupportsUnicode,
   writeStream,
@@ -59,9 +58,7 @@ class SpinniesManager {
       spinnerColor: 'greenBright',
       succeedColor: 'green',
       failColor: 'red',
-      spinner: (terminalSupportsUnicode()
-        ? SPINNERS.dots
-        : SPINNERS.dashes) as Spinner,
+      spinner: terminalSupportsUnicode() ? SPINNERS.dots : SPINNERS.dashes,
       disableSpins: false,
       ...purgeSpinnersOptions(options),
     };
