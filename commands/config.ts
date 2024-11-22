3d882c9cc7902d62d403e63038e2a4b466383548
Joe Yeager <jyeager@hubspot.com>
2024-11-22 15:03:25 -0800

    feat!: Restructure config commands to match design guidelines (#1264)

--- commands/config.ts
+++ commands/config.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck
-const { addConfigOptions, addAccountOptions } = require('../lib/commonOpts');
+const { addConfigOptions } = require('../lib/commonOpts');
+
 const { i18n } = require('../lib/lang');
 const set = require('./config/set');
 
@@ -10,8 +11,6 @@ exports.describe = i18n(`${i18nKey}.describe`);
 
 exports.builder = yargs => {
   addConfigOptions(yargs);
-  addAccountOptions(yargs);
-
   yargs.command(set).demandCommand(1, '');
 
   return yargs;
