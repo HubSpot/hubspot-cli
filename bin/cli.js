4cbedf6bf82ea253b03cef173201144f91363745
Branden Rodgers <brodgers@hubspot.com>
2024-11-22 15:25:36 -0500

    restructure hs completion command

--- bin/cli.js
+++ bin/cli.js
@@ -54,6 +54,7 @@ const sandboxesCommand = require('../commands/sandbox');
 const cmsCommand = require('../commands/cms');
 const feedbackCommand = require('../commands/feedback');
 const doctorCommand = require('../commands/doctor');
+const completionCommand = require('../commands/completion');
 
 const notifier = updateNotifier({
   pkg: { ...pkg, name: '@hubspot/cli' },
@@ -226,10 +227,10 @@ const argv = yargs
   .command(sandboxesCommand)
   .command(feedbackCommand)
   .command(doctorCommand)
+  .command(completionCommand)
   .help()
   .recommendCommands()
   .demandCommand(1, '')
-  .completion()
   .wrap(getTerminalWidth())
   .strict().argv;
 
