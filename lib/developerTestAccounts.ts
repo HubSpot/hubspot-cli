cab63c739371a76a1c582c322953f20e05ce8fa0
Camden Phalen <cphalen@hubspot.com>
2024-12-03 16:47:17 -0500

    convert lib/developerTestAccounts to TS

--- lib/developerTestAccounts.ts
+++ lib/developerTestAccounts.ts
@@ -1,31 +1,30 @@
-// @ts-nocheck
-const {
-  HUBSPOT_ACCOUNT_TYPES,
-} = require('@hubspot/local-dev-lib/constants/config');
-const {
-  getAccountId,
-  getConfigAccounts,
-} = require('@hubspot/local-dev-lib/config');
-const {
-  getAccountIdentifier,
-} = require('@hubspot/local-dev-lib/config/getAccountIdentifier');
-const { i18n } = require('./lang');
-const {
-  fetchDeveloperTestAccounts,
-} = require('@hubspot/local-dev-lib/api/developerTestAccounts');
-const {
+import { HUBSPOT_ACCOUNT_TYPES } from '@hubspot/local-dev-lib/constants/config';
+import { getAccountId, getConfigAccounts } from '@hubspot/local-dev-lib/config';
+import { getAccountIdentifier } from '@hubspot/local-dev-lib/config/getAccountIdentifier';
+import { fetchDeveloperTestAccounts } from '@hubspot/local-dev-lib/api/developerTestAccounts';
+import {
   isMissingScopeError,
   isSpecifiedError,
-} = require('@hubspot/local-dev-lib/errors/index');
-const { logger } = require('@hubspot/local-dev-lib/logger');
-const { uiAccountDescription } = require('./ui');
-const { getHubSpotWebsiteOrigin } = require('@hubspot/local-dev-lib/urls');
-const { logError } = require('./errorHandlers/index');
+} from '@hubspot/local-dev-lib/errors/index';
+import { logger } from '@hubspot/local-dev-lib/logger';
+import { getHubSpotWebsiteOrigin } from '@hubspot/local-dev-lib/urls';
+import { CLIAccount } from '@hubspot/local-dev-lib/types/Accounts';
 
-const getHasDevTestAccounts = appDeveloperAccountConfig => {
+import { i18n } from './lang';
+import { uiAccountDescription } from './ui';
+import { logError } from './errorHandlers/index';
+import { FetchDeveloperTestAccountsResponse } from '@hubspot/local-dev-lib/types/developerTestAccounts';
+import { Environment } from '@hubspot/local-dev-lib/types/Config';
+
+function getHasDevTestAccounts(appDeveloperAccountConfig: CLIAccount): boolean {
   const id = getAccountIdentifier(appDeveloperAccountConfig);
   const parentPortalId = getAccountId(id);
   const accountsList = getConfigAccounts();
+
+  if (!accountsList) {
+    return false;
+  }
+
   for (const portal of accountsList) {
     if (
       Boolean(portal.parentAccountId) &&
@@ -36,15 +35,24 @@ const getHasDevTestAccounts = appDeveloperAccountConfig => {
     }
   }
   return false;
-};
+}
 
-const validateDevTestAccountUsageLimits = async accountConfig => {
+export async function validateDevTestAccountUsageLimits(
+  accountConfig: CLIAccount
+): Promise<FetchDeveloperTestAccountsResponse | null> {
   const id = getAccountIdentifier(accountConfig);
   const accountId = getAccountId(id);
+
+  if (!accountId) {
+    return null;
+  }
+
   const { data } = await fetchDeveloperTestAccounts(accountId);
+
   if (!data) {
     return null;
   }
+
   const limit = data.maxTestPortals;
   const count = data.results.length;
   if (count >= limit) {
@@ -66,14 +74,14 @@ const validateDevTestAccountUsageLimits = async accountConfig => {
     }
   }
   return data;
-};
+}
 
-function handleDeveloperTestAccountCreateError({
-  err,
-  accountId,
-  env,
-  portalLimit,
-}) {
+export function handleDeveloperTestAccountCreateError(
+  err: unknown,
+  accountId: number,
+  env: Environment,
+  portalLimit: number
+): never {
   if (isMissingScopeError(err)) {
     logger.error(
       i18n('lib.developerTestAccount.create.failure.scopes.message', {
@@ -107,8 +115,3 @@ function handleDeveloperTestAccountCreateError({
   }
   throw err;
 }
-
-module.exports = {
-  validateDevTestAccountUsageLimits,
-  handleDeveloperTestAccountCreateError,
-};
