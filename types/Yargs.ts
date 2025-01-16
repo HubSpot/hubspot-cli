3eebc4107df1c9c4ec5461ca1c60eb36486e12f5
Branden Rodgers <brodgers@hubspot.com>
2025-01-16 16:26:45 -0500

    simplifying type

--- types/Yargs.ts
+++ types/Yargs.ts
@@ -1,8 +1,6 @@
-import { Options } from 'yargs';
-
-export interface CommonArgs extends Options {
+export type CommonArgs = {
   derivedAccountId: number;
   providedAccountId?: number;
   d: boolean;
   debug: boolean;
-}
+};
