6fda320ca2ca3e4c44de096605a240e463c7323f
Allison Kemmerle <akemmerle@hubspot.com>
2025-02-10 15:27:51 -0500

    Add basic tests

--- commands/secret.ts
+++ commands/secret.ts
@@ -1,25 +1,24 @@
-// @ts-nocheck
-const { addGlobalOptions } = require('../lib/commonOpts');
-
-const addSecretCommand = require('./secret/addSecret');
-const listSecretsCommand = require('./secret/listSecrets');
-const deleteSecretCommand = require('./secret/deleteSecret');
-const updateSecretCommand = require('./secret/updateSecret');
-const { i18n } = require('../lib/lang');
+import { Argv } from 'yargs';
+import { addGlobalOptions } from '../lib/commonOpts';
+import * as addSecretCommand from './secret/addSecret';
+import * as listSecretCommand from './secret/listSecret';
+import * as deleteSecretCommand from './secret/deleteSecret';
+import * as updateSecretCommand from './secret/updateSecret';
+import { i18n } from '../lib/lang';
 
 const i18nKey = 'commands.secret';
 
-exports.command = ['secret', 'secrets'];
-exports.describe = i18n(`${i18nKey}.describe`);
+export const command = ['secret', 'secrets'];
+export const describe = i18n(`${i18nKey}.describe`);
 
-exports.builder = yargs => {
+export function builder(yargs: Argv): Argv {
   addGlobalOptions(yargs);
 
   yargs
-    .command(listSecretsCommand)
+    .command(listSecretCommand)
     .command(addSecretCommand)
     .command(updateSecretCommand)
     .command(deleteSecretCommand)
     .demandCommand(1, '');
   return yargs;
-};
+}
