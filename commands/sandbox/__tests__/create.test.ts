97590136e74a71b938a2a396ad46190ef26919c0
Allison Kemmerle <akemmerle@hubspot.com>
2025-02-11 14:52:23 -0500

    Add basic sandbox tests

---
+++ commands/sandbox/__tests__/create.test.ts
@@ -0,0 +1,49 @@
+import yargs, { Argv } from 'yargs';
+import {
+  addAccountOptions,
+  addConfigOptions,
+  addUseEnvironmentOptions,
+  addTestingOptions,
+} from '../../../lib/commonOpts';
+
+jest.mock('yargs');
+jest.mock('../../../lib/commonOpts');
+
+// Import this last so mocks apply
+import * as sandboxCreateCommand from '../create';
+
+describe('commands/sandbox/create', () => {
+  const yargsMock = yargs as Argv;
+
+  describe('command', () => {
+    it('should have the correct command structure', () => {
+      expect(sandboxCreateCommand.command).toEqual('create');
+    });
+  });
+
+  describe('describe', () => {
+    it('should provide a description', () => {
+      expect(sandboxCreateCommand.describe).toBeDefined();
+    });
+  });
+
+  describe('builder', () => {
+    it('should support the correct options', () => {
+      sandboxCreateCommand.builder(yargsMock);
+
+      expect(yargsMock.example).toHaveBeenCalledTimes(1);
+
+      expect(addTestingOptions).toHaveBeenCalledTimes(1);
+      expect(addTestingOptions).toHaveBeenCalledWith(yargsMock);
+
+      expect(addAccountOptions).toHaveBeenCalledTimes(1);
+      expect(addAccountOptions).toHaveBeenCalledWith(yargsMock);
+
+      expect(addConfigOptions).toHaveBeenCalledTimes(1);
+      expect(addConfigOptions).toHaveBeenCalledWith(yargsMock);
+
+      expect(addUseEnvironmentOptions).toHaveBeenCalledTimes(1);
+      expect(addUseEnvironmentOptions).toHaveBeenCalledWith(yargsMock);
+    });
+  });
+});
---
+++ commands/sandbox/__tests__/delete.test.ts
@@ -0,0 +1,49 @@
+import yargs, { Argv } from 'yargs';
+import {
+  addAccountOptions,
+  addConfigOptions,
+  addUseEnvironmentOptions,
+  addTestingOptions,
+} from '../../../lib/commonOpts';
+
+jest.mock('yargs');
+jest.mock('../../../lib/commonOpts');
+
+// Import this last so mocks apply
+import * as sandboxDeleteCommand from '../delete';
+
+describe('commands/sandbox/delete', () => {
+  const yargsMock = yargs as Argv;
+
+  describe('command', () => {
+    it('should have the correct command structure', () => {
+      expect(sandboxDeleteCommand.command).toEqual('delete');
+    });
+  });
+
+  describe('describe', () => {
+    it('should provide a description', () => {
+      expect(sandboxDeleteCommand.describe).toBeDefined();
+    });
+  });
+
+  describe('builder', () => {
+    it('should support the correct options', () => {
+      sandboxDeleteCommand.builder(yargsMock);
+
+      expect(yargsMock.example).toHaveBeenCalledTimes(1);
+
+      expect(addTestingOptions).toHaveBeenCalledTimes(1);
+      expect(addTestingOptions).toHaveBeenCalledWith(yargsMock);
+
+      expect(addAccountOptions).toHaveBeenCalledTimes(1);
+      expect(addAccountOptions).toHaveBeenCalledWith(yargsMock);
+
+      expect(addConfigOptions).toHaveBeenCalledTimes(1);
+      expect(addConfigOptions).toHaveBeenCalledWith(yargsMock);
+
+      expect(addUseEnvironmentOptions).toHaveBeenCalledTimes(1);
+      expect(addUseEnvironmentOptions).toHaveBeenCalledWith(yargsMock);
+    });
+  });
+});
