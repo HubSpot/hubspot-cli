f75e286a982c676f722d04f9ffb8ad0696037433
Branden Rodgers <branden@rodgersworld.com>
2025-04-09 13:37:29 -0400

    Revert "Move middleware to folder and add tests" (#1437)

--- lib/middleware/__test__/notificationsMiddleware.test.ts
+++
@@ -1,9 +0,0 @@
-const { notifyAboutUpdates } = require('../notificationsMiddleware');
-
-describe('lib/middleware/notificationsMiddleware', () => {
-  describe('notifyAboutUpdates()', () => {
-    it('should safely execute without throwing an error', () => {
-      expect(() => notifyAboutUpdates()).not.toThrow();
-    });
-  });
-});
