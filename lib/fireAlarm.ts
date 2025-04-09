a9363af9672eb13cd5cb50116d86d6d7d87167f4
Branden Rodgers <branden@rodgersworld.com>
2025-04-09 10:05:38 -0400

    slightly more robust

--- lib/fireAlarm.ts
+++ lib/fireAlarm.ts
@@ -4,80 +4,87 @@ import { logger } from '@hubspot/local-dev-lib/logger';
 import { debugError } from './errorHandlers';
 
 /*
- * The messages in the Fire Alarms will be formatted like this:
- * ::=7.2.2::
- * This is the body for the message
- *
- * We use the :: separator to extract the version and the message body
- *
  * Versions can be formatted like this:
- * ::=7.2.2:: -> targets the exact version 7.2.2
- * ::=7.2.*:: -> targets all versions with a major of 7 and a minor of 2
- * ::=7.*:: -> targets all versions with a major of 7
- * ::=*:: -> targets all versions
- * ::<7.2.2:: -> targets all versions equal to or less than 7.2.2
- * ::<7.2.*:: -> targets all versions equal to or less than 7.2
+ * =7.2.2 -> targets the exact version 7.2.2
+ * =7.2.* -> targets all versions with a major of 7 and a minor of 2
+ * =7.* -> targets all versions with a major of 7
+ * =* -> targets all versions
+ * <=7.2.2 -> targets all versions equal to or less than 7.2.2
+ * <=7.2.* -> targets all versions equal to or less than 7.2
  */
-const SEPARATOR = '::';
-const VERSION_REGEX = new RegExp(`${SEPARATOR}(.*?)${SEPARATOR}`);
-const MESSAGE_REGEX = new RegExp(`${SEPARATOR}.*?${SEPARATOR}(.*)`, 's');
-const ANY_VERSION = '*';
+const VERSION_WILDCARD = '*';
 
-const isVersionTargeted = (
+function isVersionTargeted(
   version: string,
-  targetVersionString?: string
-): boolean => {
-  // Assume we're targeting all versions if no version string is found
+  targetVersionString: string | null
+): boolean {
+  // Assume we're targeting no versions if no version string is found
+  // Target all versions using the * wildcard
   if (!targetVersionString) {
-    return true;
+    return false;
   }
 
-  const containsOperator =
-    targetVersionString.startsWith('<') || targetVersionString.startsWith('=');
+  // Ignore alerts on any version tags (like -beta)
+  // No need for us to send alerts for these beta CLI versions
+  if (version.includes('-') || targetVersionString.includes('-')) {
+    return false;
+  }
 
-  // Assume equals if no operator is found
-  const targetVersionsLessThanOrEqualTo =
-    containsOperator && targetVersionString[0] === '<';
+  // Only support version targeting for the <= or = operator
+  if (
+    !targetVersionString.startsWith('<=') ||
+    !targetVersionString.startsWith('=')
+  ) {
+    return false;
+  }
 
-  const targetVersionStringWithoutOperator = containsOperator
-    ? targetVersionString.substring(1)
-    : targetVersionString;
-
-  // Ignore any version tags (like -beta)
-  const targetVersion = targetVersionStringWithoutOperator.split('-')[0];
+  const targetVersion = targetVersionString.substring(
+    targetVersionString.indexOf('=') + 1
+  );
 
   const targetVersionParts = targetVersion.split('.');
   const versionParts = version.split('.');
+
+  let targetAnyVersion = false;
 
   return targetVersionParts.every((targetPart, i) => {
     const versionPart = versionParts[i];
 
     // Support generic version targeting (like 1.2.*)
-    if (targetPart === ANY_VERSION) {
+    if (targetPart === VERSION_WILDCARD || targetAnyVersion) {
+      targetAnyVersion = true;
       return true;
     }
 
-    return targetVersionsLessThanOrEqualTo
-      ? versionPart <= targetPart
-      : versionPart === targetPart;
-  });
-};
+    // Double check that the target part is a number
+    if (isNaN(Number(targetPart))) {
+      return false;
+    }
 
-const filterFireAlarm = (
+    return targetVersionString.startsWith('<=')
+      ? Number(versionPart) <= Number(targetPart)
+      : Number(versionPart) === Number(targetPart);
+  });
+}
+
+function filterFireAlarm(
   fireAlarm: FireAlarm,
   command: string,
   version: string
-) => {
-  const targetVersionString = fireAlarm.message.match(VERSION_REGEX)?.[1];
+): boolean {
   const targetCommands = fireAlarm.querySelector
-    ? fireAlarm.querySelector.split(SEPARATOR)
+    ? fireAlarm.querySelector.split(',')
     : null;
 
-  const commandIsTargeted = !targetCommands || targetCommands.includes(command);
-  const versionIsTargeted = isVersionTargeted(version, targetVersionString);
+  const commandIsTargeted =
+    !targetCommands || targetCommands.some(cmd => command.startsWith(cmd));
+  const versionIsTargeted = isVersionTargeted(
+    version,
+    fireAlarm.urlRegexPattern
+  );
 
   return commandIsTargeted && versionIsTargeted;
-};
+}
 
 async function getFireAlarms(
   accountId: number,
@@ -89,6 +96,7 @@ async function getFireAlarms(
   try {
     const { data: fireAlarms } = await fetchFireAlarms(accountId);
 
+    console.log('fireAlarms', fireAlarms);
     relevantAlarms = fireAlarms.filter(fireAlarm =>
       filterFireAlarm(fireAlarm, command, version)
     );
@@ -99,15 +107,10 @@ async function getFireAlarms(
   return relevantAlarms;
 }
 
-function logFireAlarm(alarm: FireAlarm) {
-  const messageContainsVersion = !!alarm.message.match(VERSION_REGEX);
-  const formattedMessage = messageContainsVersion
-    ? alarm.message.match(MESSAGE_REGEX)?.[1]
-    : alarm.message;
-
-  if (alarm.title && formattedMessage) {
+function logFireAlarm(alarm: FireAlarm): void {
+  if (alarm.title && alarm.message) {
     logger.warn(alarm.title);
-    logger.log(formattedMessage.trim());
+    logger.log(alarm.message.trim());
     logger.log();
   }
 }
