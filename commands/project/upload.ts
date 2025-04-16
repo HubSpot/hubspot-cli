44247f04f24eec86ea565ef4386aa8325f88c775
Branden Rodgers <branden@rodgersworld.com>
2025-04-16 18:21:18 -0400

    fix: Fix process exit (#1454)

--- commands/project/upload.ts
+++ commands/project/upload.ts
@@ -120,7 +120,7 @@ export async function handler(
     );
     process.exit(EXIT_CODES.ERROR);
   }
-  process.exit(EXIT_CODES.ERROR);
+  process.exit(EXIT_CODES.SUCCESS);
 }
 
 function projectUploadBuilder(yargs: Argv): Argv<ProjectUploadArgs> {
