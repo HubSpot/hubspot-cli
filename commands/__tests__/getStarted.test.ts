1ef19930711fe102c40c2a1cd9dee9f137667660
Jorge Venegas <jvenegas@hubspot.com>
2025-05-08 11:44:54 -0700

    clean test

--- commands/__tests__/getStarted.test.ts
+++ commands/__tests__/getStarted.test.ts
@@ -1,14 +1,4 @@
-// @ts-nocheck
-
-jest.mock('@hubspot/local-dev-lib/logger');
-jest.mock('../lib/usageTracking');
-jest.mock('../lib/commonOpts', () => ({
-  addGlobalOptions: jest.fn(),
-}));
-
-// Import this last so mocks apply
 import * as getStartedCommand from '../getStarted';
-import { addGlobalOptions } from '../lib/commonOpts';
 
 describe('commands/get-started', () => {
   describe('command', () => {
@@ -20,14 +10,6 @@ describe('commands/get-started', () => {
   xdescribe('describe', () => {
     it('should provide a description', () => {
       expect(getStartedCommand.describe).toBeDefined();
-    });
-  });
-
-  describe('builder', () => {
-    it('should add global options to yargs', () => {
-      const mockYargs = {};
-      getStartedCommand.builder(mockYargs);
-      expect(addGlobalOptions).toHaveBeenCalledWith(mockYargs);
     });
   });
 });
