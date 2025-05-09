0f5b4bf0bb2dd4848d1e406bed42e87f4c979d86
Allison Kemmerle <akemmerle@hubspot.com>
2025-05-09 12:36:22 -0400

    Correct typing command bucket

--- commands/function.ts
+++ commands/function.ts
@@ -4,13 +4,27 @@ import list from './function/list';
 const deploy = require('./function/deploy');
 const server = require('./function/server');
 import { i18n } from '../lib/lang';
+import { makeYargsBuilder } from '../lib/yargsUtils';
 
 export const command = ['function', 'functions'];
 export const describe = i18n(`commands.function.describe`);
 
-export function builder(yargs: Argv): Argv {
+function functionBuilder(yargs: Argv): Argv {
   addGlobalOptions(yargs);
   yargs.command(list).command(deploy).command(server).demandCommand(1, '');
-
   return yargs;
 }
+
+const builder = makeYargsBuilder(functionBuilder, command, describe);
+
+const functionCommand = {
+  command,
+  describe,
+  builder,
+  handler: () => {},
+};
+
+export default functionCommand;
+
+// TODO Remove this legacy export once we've migrated all commands to TS
+module.exports = functionCommand;
