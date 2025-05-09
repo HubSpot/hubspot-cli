cf36a91d7c49ceae5da104007d00923ae40f503b
Camden Phalen <camdenphalen@gmail.com>
2025-05-09 12:41:14 -0400

    Setup hs create asset TS + convert api-sample (#1489)

---
+++ types/Cms.ts
@@ -0,0 +1,28 @@
+// TODO: Add real type
+export type CreateArgs = object;
+
+export type CmsAssetOperationArgs = {
+  assetType: string;
+  name: string;
+  dest: string;
+  getInternalVersion: boolean;
+  commandArgs: CreateArgs;
+};
+
+export type CreatableCmsAsset = {
+  hidden: boolean;
+  dest: (args: CmsAssetOperationArgs) => string;
+  validate: (args: CmsAssetOperationArgs) => boolean;
+  execute: (args: CmsAssetOperationArgs) => Promise<void>;
+};
+
+export type ApiSampleChoice = {
+  name: string;
+  description: string;
+  id: string;
+  languages: string[];
+};
+
+export type ApiSampleConfig = {
+  samples: ApiSampleChoice[];
+};
